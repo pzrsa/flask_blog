f9f24a98b5b7a1e95ca1873b04c10934085bc8d0
Parsa <parsamesg@gmail.com>
2021-02-26 23:23:23 +0000

    seperate run.py file and changed imports

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f341fe3e9400ea4d02d88ec4da4539f3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
