62d357a564813088ffdc25897a302437d10511c9
Parsa <parsamesg@gmail.com>
2021-02-26 23:06:28 +0000

    Initial commit in existing project

---
+++ flaskblog.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+from models import User, Post
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f341fe3e9400ea4d02d88ec4da4539f3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+posts = [
+    {
+        "author": "Parsa Mesgarha",
+        "title": "Blog Post 1",
+        "content": "First Post Content",
+        "date_posted": "Feb 25 2021"
+    },
+    {
+        "author": "John Appleseed",
+        "title": "Blog Post 2",
+        "content": "Second Post Content",
+        "date_posted": "Feb 26 2021"
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash("You have been logged in!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check the username and password.', 'danger')
+    return render_template('login.html', title="Login", form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
