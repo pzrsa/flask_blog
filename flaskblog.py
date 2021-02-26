09565ac3cd2005476f391ecd3709fe26d6d40fe2
Parsa <parsamesg@gmail.com>
2021-02-26 23:13:24 +0000

    New flaskblog folder with __init__.py file

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from models import User, Post
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f341fe3e9400ea4d02d88ec4da4539f3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+from models import User, Post
 
 
 posts = [
