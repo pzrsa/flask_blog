b6ad5f33e076a9c3afb21fbf7709b13dda199bcb
Parsa <parsamesg@gmail.com>
2021-02-27 22:22:31 +0000

    user authentication improved

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
+from flask_login import login_user
 
 
 
@@ -46,9 +47,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'pass':
-            flash("You have been logged in!", "success")
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login unsuccessful. Please check the username and password.', 'danger')
+            flash('Login unsuccessful. Please check the email and password.', 'danger')
     return render_template('login.html', title="Login", form=form)
