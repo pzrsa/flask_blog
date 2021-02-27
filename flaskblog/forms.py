e095156c43098e8c5d387e8b2fd85d3b6f77316c
Parsa <parsamesg@gmail.com>
2021-02-27 22:10:07 +0000

    Checks if user or email is already taken function

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -12,9 +13,15 @@ class RegistrationForm(FlaskForm):
 
     submit = SubmitField('Sign Up')
 
-    def validate_field(self, field):
-        if True:
-            raise ValidationError('Validation Message')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('The username is already taken. Please choose a different one.')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('The email is already taken. Please choose a different one.')        
 
 
 class LoginForm(FlaskForm):
