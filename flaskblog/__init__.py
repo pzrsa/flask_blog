7a2916d6bf1d818470c11e62bedc71b6835c66bc
Parsa <parsamesg@gmail.com>
2021-02-27 21:37:41 +0000

    imported bcrypt extension

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f341fe3e9400ea4d02d88ec4da4539f3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
