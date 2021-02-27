574df34306feaa4218dd41a836b9c845cbae1fe3
Parsa <parsamesg@gmail.com>
2021-02-27 22:30:22 +0000

    Shows logout in nav bar and logs out user if logged in

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 
 
@@ -33,6 +33,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -45,6 +47,8 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -54,3 +58,8 @@ def login():
         else:
             flash('Login unsuccessful. Please check the email and password.', 'danger')
     return render_template('login.html', title="Login", form=form)
+
+@app.route('/logout')
+def logout():
+    login_user()
+    return redirect(url_for('home'))
