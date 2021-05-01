93d186ab565e03b90a18ce1b74058c890e8434fd
Parsa <parsamesg@gmail.com>
2021-05-01 16:56:39 +0100

    fix imports

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -23,4 +23,10 @@ app.config['MAIL_USERNAME'] = os.environ.get('FLASK_EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('FLASK_EMAIL_PASS')
 mail = Mail(app)
 
-from flaskblog import routes
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
