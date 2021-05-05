d3b58abf821f9077d188fb80d3773389ae9b80b8
Parsa <parsamesg@gmail.com>
2021-05-05 13:09:41 +0100

    change database to heroku

--- flaskify/config.py
+++ flaskify/config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('FLASKIFY_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('FLASK_EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('FLASK_EMAIL_PASS')
+    MAIL_USERNAME = os.environ.get('FLASKIFY_EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('FLASKIFY_EMAIL_PASS')
